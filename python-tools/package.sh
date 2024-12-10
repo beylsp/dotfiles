6ca6f3ba36d854433fcf45c5bdf5ea46c2084353
Patrik Beyls <beylsp@yahoo.com>
2024-12-10 23:06:30 +0100

    update python-tools

--- python-tools/package.sh
+++ python-tools/package.sh
@@ -6,10 +6,12 @@ function install_package() {
     pipx install poetry
     pipx install ruff
     pipx install mypy
+    pipx install uv
 }
 
 function uninstall_package() {
     pipx uninstall poetry
     pipx uninstall ruff
     pipx uninstall mypy
+    pipx uninstall uv
 }
