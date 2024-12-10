174a4c1da5254469edb8d093fd971a03ad98d568
Patrik Beyls <beylsp@yahoo.com>
2024-12-10 23:03:29 +0100

    update python-tools

--- python-tools/package.sh
+++ python-tools/package.sh
@@ -5,9 +5,11 @@ function dependencies() {
 function install_package() {
     pipx install poetry
     pipx install ruff
+    pipx install mypy
 }
 
 function uninstall_package() {
     pipx uninstall poetry
     pipx uninstall ruff
+    pipx uninstall mypy
 }
