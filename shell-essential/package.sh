d360e8c6d344d92c40bf3a563f05a0679a9978f3
Patrik Beyls <beylsp@yahoo.com>
2024-12-10 23:26:34 +0100

    add shell essentials

---
+++ shell-essential/package.sh
@@ -0,0 +1,11 @@
+function dependencies() {
+    echo "git-config"
+}
+
+function install_package() {
+    echo "Nothing to install"
+}
+
+function uninstall_package() {
+    echo "Nothing to uninstall"
+}
